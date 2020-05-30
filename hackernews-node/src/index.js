6b8a84e93b57ff19e173e0b99f93fd287793e4f8
Joaquim <juakacc@gmail.com>
2020-05-30 09:48:06 -0300

    Connecting with database

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,61 +1,51 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-  {
-    id: "link-0",
-    description: "Fullstack tutorial for GraphQL",
-    url: "www.howtographql.com",
-  },
-];
-
-const idCount = links.length;
+const { prisma } = require("./generated/prisma-client");
 
 const resolvers = {
   Query: {
     info: () => "This is the API of a Hackernews clone",
-    feed: () => links,
-    link: (parent, args) => {
-      const link = links.find((link) => link.id === args.id);
-      return link || null;
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
     },
+    // link: (parent, args) => {
+    //   const link = links.find((link) => link.id === args.id);
+    //   return link || null;
+    // },
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      };
-      links.push(link);
-      return link;
+        description: args.description,
+      });
     },
-    updateLink: (parent, args) => {
-      const id = args.id;
-      const link = links.find((link) => link.id === id);
-      if (link) {
-        links = links.map((item) => {
-          if (item.id === id) {
-            item.url = args.url;
-            item.description = args.description;
-          }
-          return item;
-        });
-        console.log(links);
-        return links.find((item) => item.id === id);
-      } else {
-        return null;
-      }
-    },
-    deleteLink: (parent, args) => {
-      const id = args.id;
-      const link = links.find((link) => link.id === id);
-      if (link) {
-        links = links.filter((link) => link.id !== id);
-        return link;
-      } else {
-        return null;
-      }
-    },
+    // updateLink: (parent, args) => {
+    //   const id = args.id;
+    //   const link = links.find((link) => link.id === id);
+    //   if (link) {
+    //     links = links.map((item) => {
+    //       if (item.id === id) {
+    //         item.url = args.url;
+    //         item.description = args.description;
+    //       }
+    //       return item;
+    //     });
+    //     console.log(links);
+    //     return links.find((item) => item.id === id);
+    //   } else {
+    //     return null;
+    //   }
+    // },
+    // deleteLink: (parent, args) => {
+    //   const id = args.id;
+    //   const link = links.find((link) => link.id === id);
+    //   if (link) {
+    //     links = links.filter((link) => link.id !== id);
+    //     return link;
+    //   } else {
+    //     return null;
+    //   }
+    // },
   },
   Link: {
     id: (parent) => parent.id,
@@ -67,6 +57,7 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
+  context: { prisma },
 });
 
 server.start(() => console.log("Server is running on http://localhost:4000"));
